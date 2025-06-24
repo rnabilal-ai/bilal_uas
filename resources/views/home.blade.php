4d0bac6d968239200025885a4e30719ce85f3a75
rnabilal-ai <rifqinabilal25@gmail.com>
2025-06-24 14:52:03 +0700

    home ikutin siR

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,37 @@
-@extends('layouts.app')
+
+@extends('layouts.template')
+@section('title')
+    Halaman Dashboard
+@endsection
+
+@section('headline')
+    Dashboard
+@endsection
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Title</h3>
+
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+              <i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+              <i class="fas fa-times"></i>
+            </button>
+          </div>
+        </div>
+        <div class="card-body">
+          Selamat Datang Admin, semoga harimu menyenangkan
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+          Footer
+        </div>
+@endsection
+
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -15,9 +48,10 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    Hello World
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
