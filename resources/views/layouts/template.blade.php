737026f2122a673d7d5d8b954e2b6816329f0aaf
rnabilal-ai <rifqinabilal25@gmail.com>
2025-07-09 22:10:07 +0700

    perbaiki

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -185,11 +185,11 @@
       </div>
     </div>
   </div>
-  <script src="{{asset('/libs/jquery/dist/jquery.min.js')}}"></script>
-  <script src="{{asset('/libs/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{asset('/libs/simplebar/dist/simplebar.js')}}"></script>
-  <script src="{{asset('/js/sidebarmenu.js')}}"></script>
-  <script src="{{asset('/js/app.min.js')}}"></script>
+  <script src="{{asset('/assets/libs/jquery/dist/jquery.min.js')}}"></script>
+  <script src="{{asset('/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('/assets/libs/simplebar/dist/simplebar.js')}}"></script>
+  <script src="{{asset('/assets/js/sidebarmenu.js')}}"></script>
+  <script src="{{asset('/assets/js/app.min.js')}}"></script>
   <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
 </body>
 
