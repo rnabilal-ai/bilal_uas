f656570737c2e36f5f1823e3d881d0eab311f443
rnabilal-ai <rifqinabilal25@gmail.com>
2025-07-12 20:16:50 +0700

    mempercantik

--- resources/views/Pelanggan/index.blade.php
+++ resources/views/Pelanggan/index.blade.php
@@ -180,15 +180,6 @@
                                 <h3 class="card-title">
                                     <a href="/pelanggan/tambah" class="btn btn-primary btn-sm"><i class="fa fa-user-plus"></i> Tambah Data</a>
                                 </h3>
-
-                                <div class="card-tools">
-                                    <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                                        <i class="fas fa-minus"></i>
-                                    </button>
-                                    <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                                        <i class="fas fa-times"></i>
-                                    </button>
-                                </div>
                             </div>
                             <div class="card-body">
                                 <table class="table">
